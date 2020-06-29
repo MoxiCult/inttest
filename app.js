ce941fb7e97ba9cbb328194909269e0235be3fca
Jake <jake@postmyproject.com>
2020-06-29 12:54:21 -0400

    connection tests complete

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const post1comments = require('./data/post1comments')
+const album1photos = require('./data/album1photos')
+const users1todos = require('./data/users1todos')
+const users1posts = require('./data/users1posts')
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +14,18 @@ app.get('/',(req, res)=>{
 app.get('/posts/1/comments', (req, res)=>{
     res.json(post1comments)
 });
+
+app.get('/album/1/photos', (req,res)=>{
+    res.json(album1photos);
+})
+
+app.get('/users/1/todos', (req,res)=>{
+    res.json(users1todos);
+})
+
+app.get('/users/1/posts', (req,res)=>{
+    res.json(users1posts);
+})
 
 app.listen(port, ()=>console.log(`Supertest app listening at http://localhost:${port}`))
 
--- tests/apitests.js
+++ tests/apitests.js
@@ -3,8 +3,9 @@ const express = require('express');
 
 const app = require('../app.js')
 
+// posts 1 api test
 request(app)
-    .get('/posts/1/comments', ()=>{console.log('test')})
+    .get('/posts/1/comments')
     .expect('Content-type', /json/)
     .expect('Content-Length', '1329')
     .expect(200)
@@ -13,6 +14,53 @@ request(app)
             console.log(err)
         }
         else{ 
-            console.log('success!')
+            console.log('Posts api connection test succeeded')
+        }
+    })
+
+// album photos api test
+request(app)
+    .get('/album/1/photos')
+    .expect('Content-type', /json/)
+    .expect('Content-Length', '8929')
+    .expect(200)
+    .end((err,res)=>{
+        if(err){
+            console.log(err)
+        }
+        else{ 
+            console.log('Album photos api connection test succeeded')
+        }
+    })
+
+// user todo testing
+
+request(app)
+    .get('/users/1/todos')
+    .expect('Content-type', /json/)
+    .expect('Content-Length', '1670')
+    .expect(200)
+    .end((err,res)=>{
+        if(err){
+            console.log(err)
+        }
+        else{ 
+            console.log('User todo api connection test succeeded')
+        }
+    })
+
+// user posts testing
+
+request(app)
+    .get('/users/1/posts')
+    .expect('Content-type', /json/)
+    .expect('Content-Length', '2425')
+    .expect(200)
+    .end((err,res)=>{
+        if(err){
+            console.log(err)
+        }
+        else{ 
+            console.log('User posts api connection test succeeded')
         }
     })
